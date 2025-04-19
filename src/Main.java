c5b946f9d38e33d056c9225453534d7b728e2ac0
HomeWorld <KateLusis@gmail.com>
2025-04-19 20:42:02 +0300

    Add homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Author authorOne = new Author("Василий", "Пупкин");
+        Author authorTwo = new Author("Макс", "Фрай");
+        Book bookOne = new Book("Жизнь и подвиги Пупкина", authorOne, 2022);
+        Book bookTwo = new Book("Лабиринты Мёнина", authorTwo, 2015);
 
+        System.out.println("bookOne = " + bookOne);
+        System.out.println("bookTwo = " + bookTwo);
+        bookTwo.setPublicationYear(1995);
+        System.out.println("bookTwo.getPublicationYear() = " + bookTwo.getPublicationYear());
     }
 }
